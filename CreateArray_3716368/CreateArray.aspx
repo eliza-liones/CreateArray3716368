7af5f97582cbf818df3cac3d4d353922d6c0b837
Elizabeth <estrellitacalderon128@gmail.com>
2025-03-02 20:31:11 -0600

    Primer Commit

---
+++ CreateArray_3716368/CreateArray.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CreateArray.aspx.cs" Inherits="CreateArray_3716368.CreateArray" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Font-Bold="true" Text="Create Category Array"></asp:Label>
+            <br />
+            <p>
+                <asp:Button ID="btnCreateArray" runat="server" Text="Create Array" OnClick="btnCreateArray_Click" />
+            </p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CreateArray_3716368/CreateArray.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CreateArray_3716368
+{
+	public partial class CreateArray : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+        protected void btnCreateArray_Click(object sender, EventArgs e)
+        {
+            // Declare and initialize the category array.
+            string[] strCategoryArray = new string[5]
+            {
+                   "Footwear - Men's",
+                   "Clothing - Men's",
+                   "Racquets",
+                   "Footwear - Women's",
+                   "Clothing - Women's"
+            };
+
+            // Save the array in a session variable for future use.
+            Session["strCategoryArray"] = strCategoryArray;
+
+            // Redirect to the next page.
+            Response.Redirect("CategoryUseArraySessionVariables.aspx");
+        }
+    }
+}
